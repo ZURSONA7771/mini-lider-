ee79ccf7d513bbbeff6121da2dbcc5633c930901
ZURSONA777 <sayvarelo3@gmail.com>
2025-04-06 15:25:29 +0400

    commit

---
+++ day01/classwork/cw.py
@@ -0,0 +1,48 @@
+#განმარტეთ რა არის string, integer, float, boolean ტიპები. და მაგალითებიც მიუწერეთ
+
+# string == ''
+
+
+#user_num = ('please enter your name :')
+
+#print(user_num)
+
+
+#integer == normaluri ricxvebu da ara mteli 
+
+#age = 25
+#year = 2024
+#temperature = -10
+#print(age,)
+#print(year)
+#print(temperature)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#შექმენით ცვლადი სადაც თავიდან შეინახავთ სტრინგ ტიპის მონაცემს, შემდეგ ინტეჯერ ტიპის, მერე float ტიპის და ბოლოს bool ტიპის
+
+#x = "hello"  # String
+#x = 10       # Integer
+#x = 3.5      # Float
+#x = True     # Boolean
+
+
+
+
+
+#ცვლადში შეინახეთ ინტეჯერ ტიპის მონაცემი, შემდეგ გადააქციეთ float ტიპად და ისე დაპრინტეთ
+
+
+# num = 10        
+# num = float(num)  
+# print(num)     
